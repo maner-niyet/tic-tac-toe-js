ce1e631ba24c3fcf39ce34645d0d17a8881a9130
Maner Niyet <manerniyet15@gmail.com>
2022-01-15 15:19:46 -0500

    all funcs added

--- script.js
+++ script.js
@@ -4,14 +4,31 @@ const message = document.querySelector(".message")
 const xClass = "x"
 const oClass = "o"
 
+const winningCombinations = [
+    //horizontal
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    //vertical
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    //diagonal
+    [0, 4, 8],
+    [2, 4, 6]
+]
+restart.addEventListener("click", startGame)
+
 let oTurn;
 
 function startGame() {
-    oTurn = false
-    cellEls.forEach(el => {
-        el.addEventListener("click", handleClick)
+    oTurn = false;
+    message.innerHTML = ""
+    cellEls.forEach(cell => {
+        cell.innerHTML = "";
+        cell.classList.remove("x", "o")
+        cell.addEventListener("click", handleClick)
     })
-    
 }
 
 startGame()
@@ -23,6 +40,14 @@ function handleClick(e) {
     placeLetter(currentCell, currentCellTurn)
     //swapping turns
     oTurn = !oTurn
+    if (checkWinner(currentCellTurn)) {
+        message.innerHTML = `${currentCellTurn.toUpperCase()} WINNER!`
+        cellEls.forEach(cell => {
+            cell.removeEventListener("click", handleClick)
+        })
+    } else if (isDraw()) {
+        message.innerHTML = "DRAW!"
+    }
 }
 
 function placeLetter(cell, turn) {
@@ -30,5 +55,18 @@ function placeLetter(cell, turn) {
     cell.classList.add(turn)
     cell.removeEventListener("click", handleClick)
     message.innerHTML = oTurn ? "X's turn" : "O's turn"
-    
+}
+
+function checkWinner(turn) {
+    return winningCombinations.some((combination) => {
+        return combination.every(index => {
+            return cellEls[index].classList.contains(turn)
+        })
+    })
+}
+
+function isDraw() {
+    return [...cellEls].every(cell => {
+        return cell.classList.contains(xClass) || cell.classList.contains(oClass)
+    })
 }
